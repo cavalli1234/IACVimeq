4d331b1899eacb0d6e59ed198de51281cf849f0e
gianpaolo <gianpaolo.dipietro@mail.polimi.it>
2019-01-06 00:32:55 +0100

    Add requirements file + script integration for pip packages

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 conda create -n IACVimeq python=3.6 --yes
 source activate IACVimeq
-cat resources/requirements.txt | while read requirement; do
+cat resources/conda_requirements.txt | while read requirement; do
     conda install --yes ${requirement};
 done
+pip install -r resources/pip_requirements.txt
