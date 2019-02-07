b1688ad51257e4230520085961aeae7f97e22251
Luca <cavalli1234@hotmail.it>
2019-02-07 14:27:39 +0100

    sync

--- source/run/model_test.py
+++ source/run/model_test.py
@@ -9,23 +9,24 @@ from lib.models import *
 from data.loading import *
 
 if __name__ == '__main__':
-    cnn = ModelWrapper(h5_name='plain_cnn_L5.h5',
-                       model_generator=lambda: plain_cnn(layers=5))
+    # cnn = ModelWrapper(h5_name='plain_cnn_L5.h5',
+    #                    model_generator=lambda: plain_cnn(layers=5))
 
-    test_rgb = load_valid(500, shuffle=False, gray=False)
-    cnn_loss = cnn.evaluate(test_rgb, plots=4)
+    # test_rgb = load_valid(500, shuffle=False, gray=False)
+    # cnn_loss = cnn.evaluate(test_rgb, plots=4)
 
     test_gray = load_valid(500, shuffle=False, gray=True)
 
-    ff = PixwiseModelWrapper(h5_name='ddddummy_dropout.h5',
-                             model_generator=lambda: ff_hist(129))
+    ff = PixwiseModelWrapper(h5_name='ff_hist.h5',
+                             #model_generator=lambda: ff_hist(129)
+                             )
     ff_loss = ff.evaluate(test_gray, plots=4)
 
-    conv_hist = ModelWrapper(h5_name='hist_building_cnn_L5_B128.h5',
-                             model_generator=lambda: hist_building_cnn(layers=5, bins=128))
-    conv_hist_loss = conv_hist.evaluate(test_gray, plots=4)
+    # conv_hist = ModelWrapper(h5_name='hist_building_cnn_L5_B128.h5',
+    #                         model_generator=lambda: hist_building_cnn(layers=5, bins=128))
+    # conv_hist_loss = conv_hist.evaluate(test_gray, plots=4)
 
     print("Pixwise FF loss: %f" % ff_loss)
-    print("CNN loss: %f" % cnn_loss)
-    print("CNN_hist loss: %f" % conv_hist_loss)
+    # print("CNN loss: %f" % cnn_loss)
+    # print("CNN_hist loss: %f" % conv_hist_loss)
 
