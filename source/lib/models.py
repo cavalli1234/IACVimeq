b666371c6a7c768dcf8cdb8836fd587f7c94311d
gianpaolo <gianpaolo.dipietro@mail.polimi.it>
2019-02-13 14:24:21 +0100

    Add u-net

--- source/lib/models.py
+++ source/lib/models.py
@@ -1,6 +1,7 @@
 import keras.layers as kl
 import keras.models as km
 import numpy as np
+from keras.models import Model
 
 
 def dummy_cnn(channels=3, name='dummy_cnn', activation='relu'):
@@ -94,6 +95,66 @@ def hist_building_cnn(channels=1, layers=1, bins=128, name='hist_building_cnn', 
     return model
 
 
+def u_net(channels=1, name='u_net', activation='relu', dropout_rate=0.5):
+    inputs = kl.Input(shape=(None, None, channels))
+
+    # Encoding part of the network
+    conv2 = kl.Conv2D(filters=64, kernel_size=[5, 5], padding='same', kernel_initializer='glorot_normal')(inputs)
+    act2 = kl.Activation(activation)(conv2) if type(activation) is str else activation()(conv1)
+    conv2 = kl.Conv2D(filters=64, kernel_size=[5, 5], padding='same', kernel_initializer='glorot_normal')(act1)
+    act2 = kl.Activation(activation)(conv2) if type(activation) is str else activation()(conv1)
+    pool2 = kl.MaxPool2D(pool_size=[2, 2])(act2)
+
+    conv3 = kl.Conv2D(filters=128, kernel_size=[3, 3], padding='same', kernel_initializer='glorot_normal')(pool2)
+    act3 = kl.Activation(activation)(conv3) if type(activation) is str else activation()(conv3)
+    conv3 = kl.Conv2D(filters=128, kernel_size=[3, 3], padding='same', kernel_initializer='glorot_normal')(act3)
+    act3 = kl.Activation(activation)(conv3) if type(activation) is str else activation()(conv3)
+    norm3 = kl.BatchNormalization()(act3)
+    pool3 = kl.MaxPool2D(pool_size=[2, 2])(norm3)
+
+    conv4 = kl.Conv2D(filters=256, kernel_size=[3, 3], padding='same', kernel_initializer='glorot_normal')(pool3)
+    act4 = kl.Activation(activation)(conv4) if type(activation) is str else activation()(conv4)
+    conv4 = kl.Conv2D(filters=256, kernel_size=[3, 3], padding='same', kernel_initializer='glorot_normal')(act4)
+    act4 = kl.Activation(activation)(conv4) if type(activation) is str else activation()(conv4)
+    norm4 = kl.BatchNormalization()(act4)
+    drop4 = kl.Dropout(rate=dropout_rate)(norm4)
+    pool4 = kl.MaxPool2D(pool_size=[2, 2])(drop4)
+
+    conv5 = kl.Conv2D(filters=256, kernel_size=[3, 3], padding='same', kernel_initializer='glorot_normal')(pool4)
+    act5 = kl.Activation(activation)(conv5) if type(activation) is str else activation()(conv5)
+    conv5 = kl.Conv2D(filters=256, kernel_size=[3, 3], padding='same', kernel_initializer='glorot_normal')(act5)
+    act5 = kl.Activation(activation)(conv5) if type(activation) is str else activation()(conv5)
+    drop5 = kl.Dropout(rate=dropout_rate)(act5)
+
+    # Decoding part of the network
+    up6 = kl.Conv2D(filters=256, kernel_size=[3, 3], padding='same',
+                    kernel_initializer='glorot_normal')(kl.UpSampling2D(size=[2, 2])(drop5))
+    act6 = kl.Activation(activation)(up6) if type(activation) is str else activation()(up6)
+    norm6 = kl.BatchNormalization()(act6)
+    merge6 = kl.concatenate(inputs=[norm4, norm6])
+    conv6 = kl.Conv2D(filters=256, kernel_size=[3, 3], padding='same', kernel_initializer='glorot_normal')(merge6)
+    act6 = kl.Activation(activation)(conv6) if type(activation) is str else activation()(conv6)
+    conv6 = kl.Conv2D(filters=256, kernel_size=[3, 3], padding='same', kernel_initializer='glorot_normal')(act6)
+    act6 = kl.Activation(activation)(conv6) if type(activation) is str else activation()(conv6)
+
+    up7 = kl.Conv2D(filters=128, kernel_size=[3, 3], padding='same',
+                    kernel_initializer='glorot_normal')(kl.UpSampling2D(size=[2, 2])(act6))
+    act7 = kl.Activation(activation)(up7) if type(activation) is str else activation()(up7)
+    norm7 = kl.BatchNormalization()(act7)
+    merge7 = kl.concatenate(inputs=[norm3, norm7])
+    conv7 = kl.Conv2D(filters=128, kernel_size=[3, 3], padding='same', kernel_initializer='glorot_normal')(merge7)
+    act7 = kl.Activation(activation)(conv7) if type(activation) is str else activation()(conv7)
+    conv7 = kl.Conv2D(filters=128, kernel_size=[3, 3], padding='same', kernel_initializer='glorot_normal')(act7)
+    act7 = kl.Activation(activation)(conv7) if type(activation) is str else activation()(conv7)
+    drop7 = kl.Dropout(rate=dropout_rate)(act7)
+
+    # Output with one filter and sigmoid activation function
+    out = kl.Conv2D(filters=channels, kernel_size=[1, 1], activation='sigmoid', kernel_initializer='glorot_normal')(drop7)
+
+    eta_net_model = Model(inputs=(inputs,), outputs=(out,), name=name)
+    return eta_net_model
+
+
 if __name__ == '__main__':
-    model = hist_building_cnn(layers=3, bins=128)
+    model = u_net(channels=3)
     model.summary()
