8777f4cbead0af928d445544f0e7b238da542cc6
Luca <cavalli1234@hotmail.it>
2019-02-22 11:55:25 +0100

    added download of two more models

--- downloader.sh
+++ downloader.sh
@@ -3,5 +3,7 @@ wget http://46.101.188.29/ff_L10_B64.h5
 wget http://46.101.188.29/plain_cnn_L10_cifar.h5
 wget http://46.101.188.29/plain_cnn_L10_fivek.h5
 wget http://46.101.188.29/u_net_best.h5
+wget http://46.101.188.29/hist_building_cnn_L10_B128_superGood.h5
+wget http://46.101.188.29/hist_building_cnn_L5_B64_fivek.h5
 mv *.h5 resources/models/
 
